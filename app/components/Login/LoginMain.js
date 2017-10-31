295a9a0d63d337dcd1d4ab83c1d22a8ef7ee01b9
Abin <abinkhatiwada@gmail.com>
2017-10-31 08:13:58 +0545

    react router v4 added

--- app/components/Login/LoginMain.js
+++ app/components/Login/LoginMain.js
@@ -1,11 +1,24 @@
 import React from 'react';
 
 export default class LoginMain extends React.Component{
+    constructor(){
+        super();
+        this.handleFormSubmit=this.handleFormSubmit.bind(this);
+    }
+    handleFormSubmit(){
+        console.log("user name is "+this.refs.username.value);
+        console.log("password is "+this.refs.userpassword.value);
+
+    }
     render(){
         return(
             <div className="row">
-                <div className="col-md-offset-4 col-md-2">
-                    <img style={{height:"500",width:"500"}} src={require("../../resources/images/login.png")}/>
+                <div className="col-md-offset-6 col-md-2">
+                       <form onSubmit={(e)=>{e.preventDefault();this.handleFormSubmit()}}>
+                        <input className="form-control" type="text" ref="username"/>
+                        <input className="form-control" type="text" ref="userpassword"/>
+                        <input className="form-control" type="submit"/>
+                       </form>
                 </div>
             </div>
         )
